ada36087fd0a53dd9e63c86dd6b109b521b38a88
Fabian-MG <mirandafabian1510@gmail.com>
2021-06-08 22:34:59 -0500

    Accepting a function as a reducer action

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,17 +3,20 @@
 
 import * as React from 'react'
 
-const countReducer = (state, newState) => {
-  return {...state, ...newState}
+const countReducer = (state, action) => {
+  return {
+    ...state,   
+    ...(typeof action === 'function' ? action(state) : action),
+  }
 }
 
 function Counter({initialCount = 0, step = 2}) {
-  const [state, setState] = React.useReducer(countReducer,  {
-    count: initialCount,
-  })
+  const [state, setState] = React.useReducer(countReducer, {count: initialCount})
+  
   const {count} = state
+  const increment = () =>
+    setState(currentState => ({count: currentState.count + step}))
 
-  const increment = () => setState({count: count + step})
   return <button onClick={increment}>{count}</button>
 }
 
